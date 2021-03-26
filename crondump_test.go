0c27606dd065e9af3cacb90968ed52f4f578c665
Jon Barber <jon.barber@acm.org>
2021-03-26 13:27:33 +0000

    Behaviour : check minute range is not too low

--- crondump_test.go
+++ crondump_test.go
@@ -24,6 +24,7 @@ func TestMinute(t *testing.T) {
 		"range ends too high":   {input: "0-60", errExpected: true},
 		"range starts too low":  {input: "-1-59", errExpected: true},
 		"range too high":        {input: "60-61", errExpected: true},
+		"range too low":         {input: "-10--1", errExpected: true},
 	}
 
 	for desc, tc := range tests {
