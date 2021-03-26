53f7f06e5af408488a1c716f702eef20887834b0
Jon Barber <jon.barber@acm.org>
2021-03-26 10:37:08 +0000

    Minute must be 59 or lower

--- crondump_test.go
+++ crondump_test.go
@@ -8,18 +8,27 @@ import (
 func TestMinute(t *testing.T) {
 
 	tests := map[string]struct {
-		input string
-		want  string
+		input       string
+		errExpected bool
+		want        string
 	}{
-		"single valid minute": {input: "0", want: "0"},
+		"single valid minute":   {input: "0", want: "0"},
+		"minute on upper value": {input: "59", want: "59"},
+		"minute too large":      {input: "60", errExpected: true},
 	}
 
 	for desc, tc := range tests {
 		t.Run(desc, func(t *testing.T) {
-			got := Minute(tc.input)
+			got, err := Minute(tc.input)
+
+			errorReceived := err != nil
+
+			if errorReceived != tc.errExpected {
+				t.Fatalf("Unexpected error status %v ", err)
+			}
 
 			if !cmp.Equal(tc.want, got) {
-				t.Error(cmp.Diff(tc.want, got))
+				t.Fatalf(cmp.Diff(tc.want, got))
 			}
 
 		})
--- fields.go
+++ fields.go
@@ -1,6 +1,22 @@
 package crondump
 
+import (
+	"errors"
+	"strconv"
+)
+
 // Minute expands the input in cron syntax to show the minutes included
-func Minute(input string) string {
-	return input
+func Minute(input string) (string, error) {
+
+	i, err := strconv.Atoi(input)
+
+	if err != nil {
+		return "", err
+	}
+
+	if i > 59 {
+		return "", errors.New("minute can't be larger than 59")
+	}
+
+	return input, nil
 }
